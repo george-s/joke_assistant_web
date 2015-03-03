11e69ff8cb6561bf4018e21fb1f70e231c29df5c
george <george@gmac.home>
2015-03-03 23:49:02 +0000

    created clean list function to make checkbox list formatted more easily in python rather than jinja2

--- joke_assitant.py
+++ joke_assitant.py
@@ -7,22 +7,33 @@ import sys
 sys.path.append('/disk1/playpen/dev/joke_assistant/')
 from flask import Flask, session, render_template, url_for, request, redirect
 from  JA_Classes import ArticleFeed, ArticleShortList, Draft, Joke
+from urlparse import urljoin
+from werkzeug.contrib.atom import AtomFeed
+import re
 
 app = Flask("__name__",  static_folder = "images")
 print app
 app.debug = True
 app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
 
-# def sumSessionCounter():
-#   try:
-#     session['counter'] += 1
-#   except KeyError:
-#     session['counter'] = 1
-
 
 bbc_feed = ArticleFeed('bbc', "bbc.png", 'http://feeds.bbci.co.uk/news/rss.xml?edition=uk')
 dailymail_feed = ArticleFeed('dailymail', "dailymail.jpeg", 'http://www.dailymail.co.uk/news/index.rss')
 fox_feed = ArticleFeed('fox', "fox.png", 'http://feeds.foxnews.com/foxnews/latest')
+
+def CleanUpCheckBoxList(checkboxlist):
+	cleanlist=[]
+	for line in checkboxlist:
+ 		line = re.sub('[()]', '', line)
+ 		line = re.sub("u'", "", line)
+ 		line = re.sub('u"', '', line)
+ 		line = re.sub("'", "", line)
+ 		print line
+ 		# don't need dictionary necessarily
+ 		cleanlist.append(line)
+	return cleanlist
+
+
 
 @app.route('/')
 def sourceSelector():
@@ -34,45 +45,51 @@ def sourceSelector():
  		return render_template('sourceSelector.html', session=session)
 
 
+
 @app.route('/user/<name>')
 def user(name):
 	return render_template('hello.html', name=name)
-
-@app.route('/feedlist', methods=['GET', 'POST'])
-def feedlist():
-	list_id = "123"
-	# Get The Article Feed Object As list
-	chosenfeed = eval(str(session['source'])+ "_feed")
-	index_article_list = chosenfeed.articlesTitleLinkList()
-	# Handle the checkboxes array
-	if request.method == "POST":
-		if request.form.getlist("checkboxes"):
-			session['shortlistitems'] = request.form.getlist("checkboxes")
-			print "lllllllllllllll"
-		return redirect(url_for('shortlist'))
-	else:
-		print "2222222"
-		return render_template('feedlist.html', list_id=list_id, index_article_list=index_article_list, session=session)
 
 @app.route('/joke/<jokename>')
 def joke(jokename):
 	return render_template('joke.html', jokename=jokename)
 
+
+
+# Get The Article Feed Object As list Handle the checkboxes array as list
+@app.route('/feedlist', methods=['GET', 'POST'])
+def feedlist():
+	
+	chosenfeed = eval(str(session['source'])+ "_feed")
+	index_article_list = chosenfeed.articlesTitleLinkList()
+	
+	if request.method == "POST":
+		if request.form.getlist("checkboxes"):
+			session['shortlistitems'] = request.form.getlist("checkboxes")
+		return redirect(url_for('shortlist'))
+	else:
+		return render_template('feedlist.html', index_article_list=index_article_list, session=session)
+
+
+
 @app.route('/shortlist')
 def shortlist():
+	CleanUpCheckBoxList(session['shortlistitems'])
 	return render_template('shortlist.html')
 
-@app.route('/form')
-def form():
-  #sumSessionCounter()
-  # if a name has been sent, store it on a session variable
+# if a name has been sent, store it in a session variable
+# And then redirect the user to the main page
+# If no name has been sent, show the form again
+
+@app.route('/name_form')
+def name_form():
+
   if request.args.get('yourname'):
     session['name'] = request.args.get('yourname')
-    # And then redirect the user to the main page
     return redirect(url_for('sourceSelector'))
   else:
-    # If no name has been sent, show the form
-    return render_template('form.html', session=session)
+    return render_template('name_form.html', session=session)
+
 
 
 if __name__ == '__main__':
